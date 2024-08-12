d5876c21454d6d75f1aaf22a91afc6306fe4c306
Sasha Hurbanava <sasha.hurbnva@gmail.com>
2024-08-12 16:02:34 +0300

    init: complete the task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,55 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    //разбиваем на отдельные слова
+    let arrOfWords = expr.split('**********');
+    
+    let arrOfLetters = [];
+    
+    //каждое слово разбиваем на массив, в каждом элементе которого по 10 символов
+    arrOfWords.forEach(word => {
+        let arrOfLetter = [];
+        for(a=0; a<word.length; a+10){
+            let codedLetter = word.slice(a, a+=10);
+            //ищем индекс первой единицы (тем самым отсекая ненужные нули вначале)
+            let index = codedLetter.indexOf(1);
+            codedLetter = codedLetter.slice(index, codedLetter.length);
+            //делим строку на 10 и 11
+            let tens = [];
+            let arrOfTens = [];
+            for(i=0; i<codedLetter.length; i+2){
+                tens = codedLetter.slice(i, i+=2);
+                arrOfTens.push(tens);
+            }
+            arrOfLetter.push(arrOfTens);
+        }
+       arrOfLetters.push(arrOfLetter);
+    });
+
+    
+    arrOfLetters.forEach(element => {
+        for(let i = 0; i<element.length; i++){
+            for(let a = 0; a<element[i].length; a++){
+                if(element[i][a] === '10'){
+                    element[i][a] = '.';
+                }
+                else{
+                    element[i][a] = '-';
+                }
+            }
+            element[i] = element[i].join('');
+            element[i] = MORSE_TABLE[element[i]];
+        } 
+    });
+
+    let message = [];
+    for(i=0; i<arrOfLetters.length; i++){
+        let str = arrOfLetters[i].join('');
+        message.push(str);
+    }
+    return message.join(' ');
 }
+
 
 module.exports = {
     decode
